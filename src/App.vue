31ecd8c751ce715e2b73276d05963e3859f14cab
Felipe Escobedo <escobedo.felipe@hotmail.com>
2020-03-28 13:36:51 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="loader text-center" v-if="loading">
+      <div class="loader-inner">
+        <b-img :src="require('./assets/pikachu.gif')" fluid alt="pika-pika"></b-img>
+        <b-alert show variant="warning">Loading...</b-alert>
+      </div>
     </div>
+
+    <b-navbar toggleable="md" variant="danger" type="dark">
+      <b-navbar-brand href="#">PokeDex App</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Inicio</b-nav-item>
+          <b-nav-item to="/about">Liga</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input class="mr-sm-2" size="sm" placeholder="Buscar"></b-form-input>
+            <b-button class="my-2 my-sm-0" size="sm" type="submit">Buscar</b-button>
+          </b-nav-form>
+
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    start() {
+      console.log('start');
+      this.loading = true;
+    },
+    finish() {
+      setTimeout( () => {
+        this.loading = false;
+        console.log('finish');
+      }, 4000)
+    }
+  },
+  mounted() {
+    this.start();
+    this.finish();
+  },
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="css" scoped>
+.router-link-exact-active {
+  color: white !important;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.loader {
+  width: 100vw;
+  height: 100vh;
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.loader-inner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-lg">
+    <div class="row">
+      <b-card-group deck v-for="(pokemon, index) in kantoPokemon" :key="index">
+
+      </b-card-group>
+    </div>
   </div>
+    
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      kantoPokemon: [] //arreglo { name, url }
+    }
+  },
+  mounted() {
+    this.getKantoPokemon();
+  },
+  methods: {
+    async getKantoPokemon() {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .then(allPokemon => {
+          allPokemon.data.results.forEach( pokemon => {
+            this.getPokeData(pokemon);
+          })
+        })
+      } catch (error) {
+        console.error(error, 'Something went wrong!');
+      }
+    },
+    async getPokeData(pokemon) {
+      const url = pokemon.url;
+      const response = await axios.get(url)
+      .then(pokeData => console.log(pokeData)
+      )
+    }
   }
 }
 </script>
