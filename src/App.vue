3fa5a5beb7517e8fcc8e0cab010159822e1dcc4c
Felipe Escobedo <escobedo.felipe@hotmail.com>
2020-03-28 20:43:02 -0600

    axios was implemented

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,57 @@
       </div>
     </div>
 
-    <b-navbar toggleable="md" variant="danger" type="dark">
-      <b-navbar-brand href="#">PokeDex App</b-navbar-brand>
+    <b-navbar toggleable="md" variant="danger" type="dark" fixed="top">
+      <b-navbar-brand href="#">
+        PokeDex App
+      </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">Inicio</b-nav-item>
-          <b-nav-item to="/about">Liga</b-nav-item>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">League</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input class="mr-sm-2" size="sm" placeholder="Buscar"></b-form-input>
-            <b-button class="my-2 my-sm-0" size="sm" type="submit">Buscar</b-button>
+            <b-form-input class="mr-sm-2" size="sm" placeholder="
+Gotta Catch 'Em All"></b-form-input>
+            <b-button class="my-2 my-sm-0" size="sm" type="submit">Search</b-button>
           </b-nav-form>
+
+          <b-nav-item>
+            <a href="https://github.com/eosfelipe/pokedex-app" target="_blank">
+            <b-img :src="require('./assets/github.png')" class="d-inline-block align-top" fluid alt="pokeball"></b-img></a>
+          </b-nav-item>
 
 
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
     <router-view/>
+    <!--Footer-->
+    <footer class="bg-danger text-white">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-lg-6 col-md-6">
+            <p class="font-italic text-center align-middle pt-70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odio tempora inventore dolorum distinctio sapiente sequi voluptatum, maxime fugiat reprehenderit vero blanditiis unde doloribus obcaecati cum magnam qui exercitationem deserunt.</p>
+            <ul class="list-inline mt-4">
+              <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-lg-6 col-md-6 .offset-md-6 text-center">
+            <b-img class="lugia" :src="require('./assets/footer-lugia.png')" fluid alt="lugia"></b-img>
+          </div>
+        </div>
+      </div>
+      <div class="bg-light py-4">
+        <div class="container text-center">
+          <p class="text-muted mb-0 py-2">© {{ano}} @eosfelipe All rights reserved.</p>
+        </div>
+      </div>
+  </footer>
+    <!---->
   </div>
 </template>
 
@@ -35,7 +65,8 @@
 export default {
   data() {
     return {
-      loading: false
+      loading: false,
+      ano: new Date().getFullYear()
     }
   },
   methods: {
@@ -75,5 +106,12 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+.lugia {
+  width: 300px;
+  height: 300px;
+}
+.pt-70 {
+    padding-top: 70px !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="home container-lg">
-    <div class="row">
-      <b-card-group deck v-for="(pokemon, index) in kantoPokemon" :key="index">
-
-      </b-card-group>
+  <div class="container-fluid">
+    <div class="px-lg-5 pt-70">
+      <div class="row">
+        <div class="col-xl-3 col-lg-4 col-md-6 mb-4" v-for="pokemon in sortedArray" :key="pokemon.id">
+          <div class="bg-white rounded shadow-sm">
+            <b-img :src="`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`" fluid>
+            </b-img>
+            <div class="p-4">
+              <h5> <a href="#" class="text-dark">{{pokemon.name | capitalize }}</a></h5>
+              <p class="small text-muted mb-0">Base EXP: {{pokemon.base_experience}}</p>
+              <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
+                <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">JPG</span></p>
+                <div class="badge badge-danger px-3 rounded-pill font-weight-normal">new</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
     
@@ -21,20 +34,37 @@ export default {
   },
   data() {
     return {
-      kantoPokemon: [] //arreglo { name, url }
+      kantoPokemon: []
+    }
+  },
+  filters: {
+    capitalize: function(value) {
+      if(!value) return '';
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+  },
+  computed: {
+    sortedArray: function() {
+      function compare(a, b) {
+        if (a.order < b.order) return -1;
+        if (a.order > b.order) return 1;
+        return 0;
+        }
+        return this.kantoPokemon.sort(compare);
     }
   },
   mounted() {
-    this.getKantoPokemon();
+    this.$nextTick(() => {
+      this.getKantoPokemon();
+    })
   },
   methods: {
     async getKantoPokemon() {
       try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10')
-        .then(allPokemon => {
-          allPokemon.data.results.forEach( pokemon => {
-            this.getPokeData(pokemon);
-          })
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=9');
+        response.data.results.forEach(pokemon => {
+          this.getPokeData(pokemon);
         })
       } catch (error) {
         console.error(error, 'Something went wrong!');
@@ -42,10 +72,20 @@ export default {
     },
     async getPokeData(pokemon) {
       const url = pokemon.url;
-      const response = await axios.get(url)
-      .then(pokeData => console.log(pokeData)
-      )
+      try {
+        const response = await axios.get(url);
+        this.kantoPokemon.push(response.data);
+      } catch (error) {
+        console.log(error, 'Something went wrong!');
+      }
     }
   }
 }
 </script>
+
+<style lang="css">
+  .pt-70 {
+    padding-top: 70px !important;
+  }
+</style>
+
