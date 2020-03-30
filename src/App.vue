08b44d4820086a526aa460b78041732d5b34d035
Felipe Escobedo <escobedo.felipe@hotmail.com>
2020-03-29 20:28:00 -0600

    another method for faster loading was implemented

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <div class="loader text-center" v-if="loading">
       <div class="loader-inner">
-        <b-img :src="require('./assets/pikachu.gif')" fluid alt="pika-pika"></b-img>
+        <b-img
+          :src="require('./assets/pikachu.gif')"
+          fluid
+          alt="pika-pika"
+        ></b-img>
         <b-alert show variant="warning">Loading...</b-alert>
       </div>
     </div>
@@ -21,42 +25,68 @@
 
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input class="mr-sm-2" size="sm" placeholder="
-Gotta Catch 'Em All"></b-form-input>
-            <b-button class="my-2 my-sm-0" size="sm" type="submit">Search</b-button>
+            <b-form-input
+              class="mr-sm-2"
+              size="sm"
+              placeholder="
+Gotta Catch 'Em All"
+            ></b-form-input>
+            <b-button class="my-2 my-sm-0" size="sm" type="submit"
+              >Search</b-button
+            >
           </b-nav-form>
 
           <b-nav-item>
             <a href="https://github.com/eosfelipe/pokedex-app" target="_blank">
-            <b-img :src="require('./assets/github.png')" class="d-inline-block align-top" fluid alt="pokeball"></b-img></a>
+              <b-img
+                :src="require('./assets/github.png')"
+                class="d-inline-block align-top"
+                fluid
+                alt="pokeball"
+              ></b-img
+            ></a>
           </b-nav-item>
-
-
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <router-view/>
+    <router-view />
     <!--Footer-->
     <footer class="bg-danger text-white">
       <div class="container py-5">
         <div class="row">
           <div class="col-lg-6 col-md-6">
-            <p class="font-italic text-center align-middle pt-70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odio tempora inventore dolorum distinctio sapiente sequi voluptatum, maxime fugiat reprehenderit vero blanditiis unde doloribus obcaecati cum magnam qui exercitationem deserunt.</p>
+            <p class="font-italic text-center align-middle pt-70">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              odio tempora inventore dolorum distinctio sapiente sequi
+              voluptatum, maxime fugiat reprehenderit vero blanditiis unde
+              doloribus obcaecati cum magnam qui exercitationem deserunt.
+            </p>
             <ul class="list-inline mt-4">
-              <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+              <li class="list-inline-item">
+                <a href="#" target="_blank" title="twitter"
+                  ><i class="fa fa-twitter"></i
+                ></a>
+              </li>
             </ul>
           </div>
           <div class="col-lg-6 col-md-6 .offset-md-6 text-center">
-            <b-img class="lugia" :src="require('./assets/footer-lugia.png')" fluid alt="lugia"></b-img>
+            <b-img
+              class="lugia"
+              :src="require('./assets/footer-lugia.png')"
+              fluid
+              alt="lugia"
+            ></b-img>
           </div>
         </div>
       </div>
       <div class="bg-light py-4">
         <div class="container text-center">
-          <p class="text-muted mb-0 py-2">© {{ano}} @eosfelipe All rights reserved.</p>
+          <p class="text-muted mb-0 py-2">
+            © {{ ano }} @eosfelipe All rights reserved.
+          </p>
         </div>
       </div>
-  </footer>
+    </footer>
     <!---->
   </div>
 </template>
@@ -67,25 +97,25 @@ export default {
     return {
       loading: false,
       ano: new Date().getFullYear()
-    }
+    };
   },
   methods: {
     start() {
-      console.log('start');
+      console.log("start");
       this.loading = true;
     },
     finish() {
-      setTimeout( () => {
+      setTimeout(() => {
         this.loading = false;
-        console.log('finish');
-      }, 4000)
+        console.log("finish");
+      }, 4000);
     }
   },
   mounted() {
     this.start();
     this.finish();
-  },
-}
+  }
+};
 </script>
 
 <style lang="css" scoped>
@@ -112,6 +142,6 @@ export default {
   height: 300px;
 }
 .pt-70 {
-    padding-top: 70px !important;
+  padding-top: 70px !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid mt-5">
     <div class="px-lg-5 pt-70">
       <div class="row">
-        <div class="col-xl-3 col-lg-4 col-md-6 mb-4" v-for="pokemon in sortedArray" :key="pokemon.id">
-          <div class="bg-white rounded shadow-sm">
-            <b-img :src="`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`" fluid>
-            </b-img>
+        <div
+          class="col-xl-3 col-lg-4 col-md-6 mb-4"
+          v-for="pokemon in sortedArray"
+          :key="pokemon.id"
+        >
+          <div class="bg-white rounded shadow">
+            <b-img
+              :src="
+                `${pokemon.img}`
+              "
+              fluid
+            ></b-img>
             <div class="p-4">
-              <h5> <a href="#" class="text-dark">{{pokemon.name | capitalize }}</a></h5>
-              <p class="small text-muted mb-0">Base EXP: {{pokemon.base_experience}}</p>
-              <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
-                <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">JPG</span></p>
-                <div class="badge badge-danger px-3 rounded-pill font-weight-normal">new</div>
+              <h5>#{{pokemon.id}}</h5>
+              <h5>
+                <a href="#" class="text-dark">
+                  {{
+                  pokemon.name | capitalize
+                  }}
+                </a>
+              </h5>
+              <p class="small text-muted mb-0">Base EXP: {{ pokemon.base_experience }}</p>
+              <div
+                class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4"
+              >
+                <div class="badge badge-danger px-3 rounded-pill font-weight-normal">type 1</div>
+                <div class="badge badge-success px-3 rounded-pill font-weight-normal">type 2</div>
               </div>
             </div>
           </div>
@@ -19,27 +36,32 @@
       </div>
     </div>
   </div>
-    
 </template>
 
 <script>
 // @ is an alias to /src
 import axios from "axios";
 
-
 export default {
-  name: 'Home',
-  components: {
-    
-  },
+  name: "Home",
+  components: {},
   data() {
     return {
-      kantoPokemon: []
-    }
+      kantoPokemon: [],
+      test: [],
+      pokemon: 
+      {
+        id: String,
+        order: String,
+        name: String,
+        base_experience: String,
+        img: String
+      }
+    };
   },
   filters: {
     capitalize: function(value) {
-      if(!value) return '';
+      if (!value) return "";
       value = value.toString();
       return value.charAt(0).toUpperCase() + value.slice(1);
     }
@@ -50,42 +72,51 @@ export default {
         if (a.order < b.order) return -1;
         if (a.order > b.order) return 1;
         return 0;
-        }
-        return this.kantoPokemon.sort(compare);
+      }
+      return this.kantoPokemon.sort(compare);
     }
   },
   mounted() {
     this.$nextTick(() => {
       this.getKantoPokemon();
-    })
+      console.log(this.test);
+    });
   },
   methods: {
     async getKantoPokemon() {
       try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=9');
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
         response.data.results.forEach(pokemon => {
           this.getPokeData(pokemon);
-        })
+        });
       } catch (error) {
-        console.error(error, 'Something went wrong!');
+        console.error(error, "Something went wrong!");
       }
     },
     async getPokeData(pokemon) {
       const url = pokemon.url;
       try {
         const response = await axios.get(url);
-        this.kantoPokemon.push(response.data);
+        this.pokemon = new Object();
+        this.pokemon.id = response.data.id;
+        this.pokemon.order = response.data.order;
+        this.pokemon.name = response.data.name;
+        this.pokemon.base_experience = response.data.base_experience;
+        this.pokemon.img = 'https://pokeres.bastionbot.org/images/pokemon/'+response.data.id+'.png';
+        // this.test.push(this.pokemon);
+        this.kantoPokemon.push(this.pokemon);
       } catch (error) {
-        console.log(error, 'Something went wrong!');
+        console.log(error, "Something went wrong!");
       }
     }
   }
-}
+};
 </script>
 
 <style lang="css">
-  .pt-70 {
-    padding-top: 70px !important;
-  }
+.pt-70 {
+  padding-top: 70px !important;
+}
 </style>
-
